456f2a7723530fa9737f376cb381072d2f735aad
AndreGuerra123 <guerraandre@hotmail.com>
2018-12-05 00:46:00 +0000

    prior to test

--- index.js
+++ index.js
@@ -1,133 +1,173 @@
-var gridfsLockingStream = require('gridfs-locking-stream');
-var fs = require('file-system');
-var path = require('path');
-var async = require('async');
+require('async');
+var grid = require('gridfs-stream');
+var fs = require('fs-extra')
+var should = require('chai').should();
+var OID = require('mongodb').ObjectID;
 
+function validate(obj) {
+    should.exist(obj)
+}
+
+function validate_oid(id) {
+    should.be.true(OID.isValid(id), id.toString() + ' is not a valid ObjectID.');
+    return id.toString();
+}
+
+function validate_path(path) {
+    fs.pathExists(path).then(exists => {
+        should.be.true(exists)
+    }).catch(err => {
+        throw err;
+    })
+}
 
 function easyGridFS(db, driver) {
 
-    var gfs = gridfsLockingStream(db,driver);
-    var self = this;
+    var gfs = grid(db, driver);
 
     this.saveFile = function (path, name, callback) {
-        try {
-            if (typeof path == 'undefined') throw '(path) of the image is undefined in (putFile) function';
-            if (typeof name == 'undefined') throw '(name) of the image is undefined in (putFile) function';
-            // if (typeof extension == 'undefined') throw '(extension) of the image is undefined in (putFile) function';
-            gfs.createWriteStream(
-                {filename: name},
-                function (err, writestream) {
-                    if (err) callback(err, null);
-                    else {
-                        if (writestream) {
-                            fs.createReadStream(path).pipe(writestream);
 
-                            writestream.on('close', function (file) {
-                                callback(null, file);
-                            });
-                        }
-                    }
-                });
+        try {
+            validate(name)
+            validate_path(path)
+        } catch (err) {
+            callback(err, null)
         }
-        catch (err) {
-            callback(err, null);
-        }
+
+        gfs.createWriteStream({
+                filename: name
+            },
+            function (err, writestream) {
+                if (err) callback(err, null);
+                else {
+                    fs.createReadStream(path).pipe(writestream);
+                    writestream.on('close', function (file) {
+                        callback(null, file);
+                    });
+                }
+            });
     };
-    
+
     this.getFileInfo = function (id, callback) {
         try {
-            if (typeof id == 'undefined') throw '(id) of the image is undefined in (getInfoById) function';
-            gfs.findOne({_id: id}, function (err, file) {
-                if (err) callback(err, null);
-                else if (!file) throw 'File not found.'
-                else callback(null, file);
-            });
+            id = validate_oid(id)
+        } catch (err) {
+            callback(err, null)
         }
-        catch (err) {
-            callback(err, null);
-        }
+
+        gfs.findOne({
+            _id:
+        }, function (err, file) {
+            if (err) callback(err, null);
+            else if (!file) callback('File not found.', null);
+            else callback(null, file);
+        });
+
     };
-    
+
     this.getFileData = function (id, callback) {
+        
+        try {
+            id = validate_oid(id)
+        } catch (err) {
+            callback(err, null)
+        }
+
         let contentType = null;
         let data = [];
-        try {
-            if (typeof id == 'undefined') throw '(id) of the image is undefined in (getFileById) function';
-            
-            gfs.findOne({_id:id},function(err,file){
-                      if (err) throw err;
-                      else if (!file) throw 'File not found.'
-                      else{
-                      contentType = file.contentType
-                      }  
-            })
-            gfs.createReadStream({_id: id},function(err,rs) {
-                      if (err) throw err;
-                      else {
-                          readStream.on('data', function (chunk) {
-                                        data.push(chunk);
-                                    });
-                          readStream.on('end', function () {
-                                        data = Buffer.concat(data);
-                                        callback(null,'data:'+contentType+';base64,' + Buffer(data).toString('base64'));
-                                    });
-                          readStream.on('error',function(err){
-                                    throw err;
-                                    });
-                      }       
-            });
-           
-        }catch (err) {
-            callback(err, null);
-        }
+
+        gfs.findOne({
+            _id: id
+        }, function (err, file) {
+            if (err) callback(err, null);
+            else if (!file) callback('File not found.', null);
+            else {
+                contentType = file.contentType
+            }
+        })
+        gfs.createReadStream({
+            _id: id
+        }, function (err, rs) {
+            if (err) callback(err, null);
+            else {
+                rs.on('data', function (chunk) {
+                    data.push(chunk);
+                });
+                rs.on('end', function () {
+                    data = Buffer.concat(data);
+                    callback(null, 'data:' + contentType + ';base64,' + Buffer(data).toString('base64'));
+                });
+                rs.on('error', function (err) {
+                    callback(err, null);
+                });
+            }
+        });
+
     };
-    
-    
+
+
     this.getFileBase64 = function (id, callback) {
+
         try {
-            if (typeof id == 'undefined') throw '(id) of the image is undefined in (getFileById) function';
-            let data = [];
-            gfs.createReadStream({_id: id},function(err,rs) {
-                      if (err) throw err;
-                      else {
-                          readStream.on('data', function (chunk) {
-                                        data.push(chunk);
-                                    });
-                          readStream.on('end', function () {
-                                        data = Buffer.concat(data);
-                                        callback(null,Buffer(data).toString('base64'));
-                                    });
-                          readStream.on('error',function(err){
-                                    throw err;
-                                    });
-                      }       
-            });
-           
-        }catch (err) {
-            callback(err, null);
+            id = validate_oid(id)
+        } catch (err) {
+            callback(err, null)
         }
+
+        let data = [];
+
+        gfs.createReadStream({
+            _id: id
+        }, function (err, rs) {
+            if (err) callback(err, null);
+            else {
+                rs.on('data', function (chunk) {
+                    data.push(chunk);
+                });
+                rs.on('end', function () {
+                    data = Buffer.concat(data);
+                    callback(null, Buffer(data).toString('base64'));
+                });
+                rs.on('error', function (err) {
+                    callback(err, null);
+                });
+            }
+        });
+
     };
-            
+
     this.removeFile = function (id, callback) {
+
         try {
-            if (typeof id == 'undefined') throw '(id) of the image is undefined in (removeFile) function';
-            gfs.remove({_id: id}, function (err, result) {
-                if (err) callback(err, null);
-                else if (result) callback(null, result);
-            });
+            id = validate_oid(id)
+        } catch (err) {
+            callback(err, null)
         }
-        catch (err) {
-            callback(err, null);
-        }
+
+        gfs.remove({
+            _id: id
+        }, function (err, result) {
+            if (err) callback(err, null);
+            else if (result) callback(null, result);
+        });
+
     };
-  
+
     this.existFile = function (id, callback) {
-        gfs.exist({_id: id}, function (err, found) {
+        try {
+            id = validate_oid(id)
+        } catch (err) {
+            callback(err, null)
+        }
+
+        gfs.exist({
+            _id: id
+        }, function (err, found) {
             if (err) callback(err, null);
             else callback(null, found);
         });
-    };   
-    
+    };
+
 }
 
-module.exports = eassyGridFS;
+module.exports = easyGridFS;
