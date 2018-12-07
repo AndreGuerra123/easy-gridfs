2a8e5a7a0c00ba592cd4c20b53166c235550d9bf
AndreGuerra123 <guerraandre@hotmail.com>
2018-12-07 04:46:51 +0000

    working

--- index.js
+++ index.js
@@ -1,170 +1,139 @@
-require('async');
 var grid = require('gridfs-stream');
-var fs = require('fs-extra')
-var should = require('chai').should();
-var OID = require('mongodb').ObjectID;
-
-function validate(obj) {
-    should.exist(obj)
-}
-
-function validate_oid(id) {
-    should.be.true(OID.isValid(id), id.toString() + ' is not a valid ObjectID.');
-    return id.toString();
-}
-
-function validate_path(path) {
-    fs.pathExists(path).then(exists => {
-        should.be.true(exists)
-    }).catch(err => {
-        throw err;
-    })
-}
+var fs = require('fs')
+var mime = require('mime-types')
+var path = require('path')
+var OID = require("mongodb").ObjectID
 
 function easyGridFS(db, driver) {
-
     this.gfs = grid(db, driver);
-
-    this.saveFile = function (path, name, callback) {
-
-        try {
-            validate(name)
-            validate_path(path)
-        } catch (err) {
-            callback(err, null)
-        }
-
-        this.gfs.createWriteStream({
-                filename: name
-            },
-            function (err, writestream) {
-                if (err) callback(err, null);
-                else {
-                    fs.createReadStream(path).pipe(writestream);
-                    writestream.on('close', function (file) {
-                        callback(null, file);
-                    });
-                }
-            });
-    };
-
-    this.getFileInfo = function (id, callback) {
-        try {
-            id = validate_oid(id)
-        } catch (err) {
-            callback(err, null)
-        }
-
-        this.gfs.findOne({
-            _id: id
-        }, function (err, file) {
-            if (err) callback(err, null);
-            else if (!file) callback('File not found.', null);
-            else callback(null, file);
-        });
-
-    };
-
-    this.getFileData = function (id, callback) {
-        
-        try {
-            id = validate_oid(id)
-        } catch (err) {
-            callback(err, null)
-        }
-
-        let contentType = null;
-        let data = [];
-
-        this.gfs.findOne({
-            _id: id
-        }, function (err, file) {
-            if (err) callback(err, null);
-            else {
-                contentType = file.contentType
-            }
-        })
-        this.gfs.createReadStream({
-            _id: id
-        }, function (err, rs) {
-            if (err) callback(err, null);
-            else {
-                rs.on('data', function (chunk) {
-                    data.push(chunk);
-                });
-                rs.on('end', function () {
-                    data = Buffer.concat(data);
-                    callback(null, 'data:' + contentType + ';base64,' + Buffer(data).toString('base64'));
-                });
-            }
-        });
-
-    };
-
-
-    this.getFileBase64 = function (id, callback) {
-
-        try {
-            id = validate_oid(id)
-        } catch (err) {
-            callback(err, null)
-        }
-
-        let data = [];
-
-        this.gfs.createReadStream({
-            _id: id
-        }, function (err, rs) {
-            if (err) callback(err, null);
-            else {
-                rs.on('data', function (chunk) {
-                    data.push(chunk);
-                });
-                rs.on('end', function () {
-                    data = Buffer.concat(data);
-                    callback(null, Buffer(data).toString('base64'));
-                });
-                rs.on('error', function (err) {
-                    callback(err, null);
-                });
-            }
-        });
-
-    };
-
-    this.removeFile = function (id, callback) {
-
-        try {
-            id = validate_oid(id)
-        } catch (err) {
-            callback(err, null)
-        }
-
-        this.gfs.remove({
-            _id: id
-        }, function (err, result) {
-            if (err) callback(err, null);
-            else if (result) callback(null, result);
-        });
-
-    };
-
-    this.existFile = function (id, callback) {
-        try {
-            id = validate_oid(id)
-        } catch (err) {
-            callback(err, null)
-        }
-
-        this.gfs.exist({
-            _id: id
-        }, function (err, found) {
-            if (err) callback(err, null);
-            else callback(null, found);
-        });
-    };
-
     return this;
 }
+
+easyGridFS.prototype.validateOBID = function(id){
+        if(OID.isValid(id.toString())){
+            return id.toString()
+        }else{
+            throw new Error(id.toString()+' is not a valid ObjectId.')
+        }
+}
+
+easyGridFS.prototype.saveFile = async function (filepath, filename, callback) {
+    try {
+        var ws = this.gfs.createWriteStream({
+            filename: filename || path.basename(filepath),
+            mode: 'w',
+            content_type: mime.lookup(filepath) || 'application/octet-stream'
+        })
+        fs.createReadStream(filepath).pipe(ws);
+        ws.on('close', function (file) {
+            callback(null, file);
+        });
+        ws.on('error', function (err) {
+            callback(err, null)
+        })
+    } catch (err) {
+        callback(err, null);
+    }
+};
+easyGridFS.prototype.getFileInfo = function (id, callback) {
+    try {
+        var _id = this.validateOBID(id)
+        this.gfs.findOne({
+            _id
+        }, function (err, file) {
+            if (err) callback(err, null)
+            else if (!file) callback(new Error('File does not exist.'), null)
+            else callback(null, file)
+        });
+    } catch (err) {
+        callback(err, null)
+    }
+
+};
+
+easyGridFS.prototype.getFileData = async function (id, callback) {
+   
+    try {
+        var _id = this.validateOBID(id)
+        let file;
+        let data = [];
+        await this.gfs.findOne({
+           _id
+        }, function (err, f) {
+            if (err) callback(err, null)
+            else if (!f) callback(new Error('File not found.'), null)
+            else {
+                file = f
+            }
+        });
+        var rs = this.gfs.createReadStream({
+           _id
+        })
+        rs.on('data', function (chunk) {
+            data.push(chunk);
+        });
+        rs.on('end', function () {
+            data = Buffer.concat(data);
+            callback(null, 'data:' + file.contentType + ';base64,' + Buffer.from(data).toString('base64'));
+        });
+        rs.on('error', function (err) {
+            callback(err, null)
+        })
+    } catch (err) {
+        callback(err, null)
+    }
+};
+
+easyGridFS.prototype.getFileBase64 = async function (id, callback) {
+    
+    try {
+        let data = [];
+        var _id = this.validateOBID(id)
+        var rs = this.gfs.createReadStream({
+            _id
+        })
+        rs.on('data', function (chunk) {
+            data.push(chunk);
+        });
+        rs.on('end', function () {
+            data = Buffer.concat(data);
+            callback(null, Buffer.from(data).toString('base64'));
+        });
+        rs.on('error', function (err) {
+            callback(err, null)
+        })
+    } catch (err) {
+        callback(err, null)
+    }
+};
+
+easyGridFS.prototype.removeFile = function (id, callback) {
+    try {
+        var _id = this.validateOBID(id)
+        this.gfs.remove({
+            _id
+        }, function (err, _gs) {
+            if (err) callback(err)
+            else callback(null)
+        });
+    } catch (err) {
+        callback(err)
+    }
+};
+
+easyGridFS.prototype.existFile = function (id, callback) {
+    try {
+        _id = this.validateOBID(id)
+        this.gfs.exist({
+           _id
+        }, function (err, found) {
+            if (err) callback(err, null)
+            else callback(null, found)
+        });
+    } catch (err) {
+        callback(err, null)
+    }
+
+};
 
 module.exports = easyGridFS;
